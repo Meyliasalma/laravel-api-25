ef1751f7d7e803511e4feca2733fb14703fd6b9e
Meyliasalma <meyysalma27@gmail.com>
2025-10-21 08:53:54 +0700

    Refactor project structure and update configurations for Laravel 12 compatibility. Changes include renaming docker-compose file, enhancing .env.example with new locale settings, updating composer dependencies, and removing deprecated files. Adjusted logging, session, and database configurations for improved performance and security. Added Tailwind CSS integration and refined README for clarity.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/product-store', [ProductController::class, 'store']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get ('/halo',function (){
+    return 'Halo, Laravel!';
 });
+
+
