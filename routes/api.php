15e0fa4d9d3cdd87cb1d9ca9db0d79aa3ed284c6
Meyliasalma <meyysalma27@gmail.com>
2025-10-28 10:53:20 +0700

    Add VendorController routes and update ProductController routes in API

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,13 @@
 <?php
 
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
+Route::resource('product', ProductController::class);
 
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/product-store', [ProductController::class, 'store']);
+Route::resource('vendors',VendorController::class);
 
 Route::get ('/halo',function (){
     return 'Halo, Laravel!';
